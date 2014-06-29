7027bb4d6d0ec33076c2769a0303020e65ddd7ef
GTL <cclark@gametheorylabs.com>
2014-06-29 13:50:14 -0500

    Updated worker test to included Shared Worker

--- worker/worker.js
+++ worker/worker.js
@@ -2,10 +2,18 @@ self.addEventListener('message', function(e) {
   self.postMessage("Worker Says: " + e.data);
 }, false);
 
-var worker = new Worker('nested.js');
-    
-worker.addEventListener('message', function(e) {
+try{
+  var worker = new Worker('nested.js');
+  worker.addEventListener('message', function(e) {
     self.postMessage('Nested Worker said: '+ e.data);
   }, false);
+  
+}
+catch(e){
+  self.postMessage('Worker Says: Nested Worker NOT Supported');
+}
+
+    
+
 
 self.postMessage('Worker Says: Online');
